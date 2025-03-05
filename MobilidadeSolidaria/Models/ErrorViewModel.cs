e5558955faca3f85349d0cd8c0afb2a84a0176e4
RafaDiogoJ <rafabbta@hotmail.com>
2025-03-05 14:25:06 -0300

    Criação do Projeto

---
+++ MobilidadeSolidaria/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace MobilidadeSolidaria.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
