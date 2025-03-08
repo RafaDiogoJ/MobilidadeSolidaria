046a53dd98eb3cc04dd414ac04b0e6e4746b041c
RafaDiogoJ <rafabbta@hotmail.com>
2025-03-07 22:13:07 -0300

    Estilização index 07-03

--- MobilidadeSolidaria/obj/Debug/net9.0/MobilidadeSolidaria.AssemblyInfo.cs
+++ MobilidadeSolidaria/obj/Debug/net9.0/MobilidadeSolidaria.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MobilidadeSolidaria")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+595f6b7ec946a06a6112a897e8e65a89685abb35")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c074ab67aad709bd0d0291e342b03da71f99933")]
 [assembly: System.Reflection.AssemblyProductAttribute("MobilidadeSolidaria")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MobilidadeSolidaria")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
