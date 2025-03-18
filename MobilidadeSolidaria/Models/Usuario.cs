d650f47ab048af000a93d176f26cbafde1d23138
RafaDiogoJ <rafabbta@hotmail.com>
2025-03-18 20:34:11 -0300

    Correção campo email usuario

--- MobilidadeSolidaria/Models/Usuario.cs
+++ MobilidadeSolidaria/Models/Usuario.cs
@@ -3,20 +3,16 @@ using Microsoft.AspNetCore.Identity;
 
 namespace MobilidadeSolidaria.Models;
 public class Usuario : IdentityUser
-    {
+{
 
-        [Required(ErrorMessage = "Por favor, insira o nome")]
-        [StringLength(60, ErrorMessage = "O nome deve conter no máximo 60 caracteres")]
-        public string Nome { get; set; }
+    [Required(ErrorMessage = "Por favor, insira o nome")]
+    [StringLength(60, ErrorMessage = "O nome deve conter no máximo 60 caracteres")]
+    public string Nome { get; set; }
 
-        [Required(ErrorMessage = "Por favor, insira o email")]
-        [StringLength(60, ErrorMessage = "O e-mail deve conter no máximo 60 caracteres")]
-        public string Email { get; set; }
+    [Required(ErrorMessage = "Por favor, insira a senha")]
+    [StringLength(30, ErrorMessage = "a senha deve conter no máximo 30 caracteres")]
+    public string Senha { get; set; }
 
-        [Required(ErrorMessage = "Por favor, insira a senha")]
-        [StringLength(30, ErrorMessage = "a senha deve conter no máximo 30 caracteres")]
-        public string Senha { get; set; }
+    public List<Equipamento> Equipamentos { get; set; }  // Um usuário pode ter vários equipamentos
 
-        public List<Equipamento> Equipamentos { get; set; }  // Um usuário pode ter vários equipamentos
-
-    }
+}
