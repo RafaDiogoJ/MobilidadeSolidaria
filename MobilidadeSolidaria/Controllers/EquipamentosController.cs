7d8aa684ea374ed385691b81789e20d1720893a5
RafaDiogoJ <rafabbta@hotmail.com>
2025-05-02 11:06:08 -0300

    CRUD Equipamentos

---
+++ MobilidadeSolidaria/Controllers/EquipamentosController.cs
@@ -0,0 +1,170 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using MobilidadeSolidaria.Data;
+using MobilidadeSolidaria.Models;
+
+namespace MobilidadeSolidaria.Controllers
+{
+    public class EquipamentosController : Controller
+    {
+        private readonly AppDbContext _context;
+
+        public EquipamentosController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Equipamentos
+        public async Task<IActionResult> Index()
+        {
+            var appDbContext = _context.Equipamentos.Include(e => e.Categoria).Include(e => e.Usuario);
+            return View(await appDbContext.ToListAsync());
+        }
+
+        // GET: Equipamentos/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipamento = await _context.Equipamentos
+                .Include(e => e.Categoria)
+                .Include(e => e.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (equipamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipamento);
+        }
+
+        // GET: Equipamentos/Create
+        public IActionResult Create()
+        {
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "Id", "Nome");
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id");
+            return View();
+        }
+
+        // POST: Equipamentos/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("Id,Nome,Descricao,Status,Cidade,Estado,UsuarioId,CategoriaId")] Equipamento equipamento)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(equipamento);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "Id", "Nome", equipamento.CategoriaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", equipamento.UsuarioId);
+            return View(equipamento);
+        }
+
+        // GET: Equipamentos/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipamento = await _context.Equipamentos.FindAsync(id);
+            if (equipamento == null)
+            {
+                return NotFound();
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "Id", "Nome", equipamento.CategoriaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", equipamento.UsuarioId);
+            return View(equipamento);
+        }
+
+        // POST: Equipamentos/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,Descricao,Status,Cidade,Estado,UsuarioId,CategoriaId")] Equipamento equipamento)
+        {
+            if (id != equipamento.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(equipamento);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!EquipamentoExists(equipamento.Id))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CategoriaId"] = new SelectList(_context.Categoria, "Id", "Nome", equipamento.CategoriaId);
+            ViewData["UsuarioId"] = new SelectList(_context.Usuarios, "Id", "Id", equipamento.UsuarioId);
+            return View(equipamento);
+        }
+
+        // GET: Equipamentos/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var equipamento = await _context.Equipamentos
+                .Include(e => e.Categoria)
+                .Include(e => e.Usuario)
+                .FirstOrDefaultAsync(m => m.Id == id);
+            if (equipamento == null)
+            {
+                return NotFound();
+            }
+
+            return View(equipamento);
+        }
+
+        // POST: Equipamentos/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var equipamento = await _context.Equipamentos.FindAsync(id);
+            if (equipamento != null)
+            {
+                _context.Equipamentos.Remove(equipamento);
+            }
+
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool EquipamentoExists(int id)
+        {
+            return _context.Equipamentos.Any(e => e.Id == id);
+        }
+    }
+}
